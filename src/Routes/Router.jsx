5fba54e9dd6d3e6862e2cb76aa85c5a815376756
abimbolaalabi <ibrahimade92@gmail.com>
2025-10-23 23:56:04 +0100

    i worked on both runner and client signup, validated both forms and worked on responsiveness, waiting for api

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -7,13 +7,15 @@ import PageNotFound from '../pages/PageNotFound'
 import ClientVsRunner from '../pages/ClientVsRunner/ClientVsRunner'
 import ForgotPassword from "../pages/Auth/Forgotpassword/Forget"
 import ResetPassword from "../pages/Auth/Reset/ResetPassword"
+import SignUpRunner from '../pages/Auth/SignUpRunner/SignUpRunner'
 const Router = () => {
   return (
     <BrowserRouter>
     <Routes>
         <Route path='*' element={<PageNotFound/>}/>
         <Route path='/' element={<LandingPage/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/signupclient' element={<SignUp/>}/>
+        <Route path='/signuprunner' element={<SignUpRunner/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/verifyemail' element={<VerifyEmail/>}/>
         <Route path='/clientvsrunner' element={<ClientVsRunner/>}/>
---
+++ src/pages/Auth/SignUpRunner/SignUpRunner.jsx
@@ -0,0 +1,227 @@
+import React, { useState } from 'react'
+import './SignUpRunner.css'
+import Carousel from '../../../Components/Carousel/Carousel'
+import { FcGoogle } from "react-icons/fc";
+import { toast } from 'react-toastify';
+import { FaEyeSlash } from "react-icons/fa6";
+import { FaEye } from "react-icons/fa";
+
+
+const SignUpRunner = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const [formErrors, setFormErrors] = useState({})
+  const [successMessage, setSuccessMessage] = useState('')
+  const [ShowPassword, SetShowPassword] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const validateForm = () => {
+    const { fullName, lastName, email, password, confirmPassword } = formData;
+
+    if (!fullName || !lastName || !email || !password || !confirmPassword) {
+      toast.error("All fields are required");
+      return false;
+    }
+
+    if (!/^\S+@\S+\.\S+$/.test(email)) {
+      toast.error("Invalid email format");
+      return false;
+    }
+
+
+    if (password[0] !== password[0].toUpperCase()) {
+      toast.error("Password must start with an uppercase letter");
+      return false;
+    }
+
+    if (password.length < 8) {
+      toast.error("Password must be at least 8 characters long");
+      return false;
+    }
+
+    if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])/.test(password)) {
+      toast.error(
+        "Password must contain uppercase, lowercase, number, and symbol"
+      );
+      return false;
+    }
+
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return false;
+    }
+
+    return true;
+  };
+
+
+
+  return (
+    <div className="signup-container">
+      <div className="signup-left">
+        <Carousel />
+      </div>
+
+      <div className="signup-right">
+        <div className='signup-right-cont'>
+          <form>
+            <h1>Sign Up As A Runner</h1>
+            <div className="input-cont">
+              <label>First Name</label>
+              <input
+                type="text"
+                name="firstName"
+                placeholder="Enter your first name"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="input-cont">
+              <label>Last Name</label>
+              <input type="text"
+                placeholder="Enter your last name"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="input-cont">
+              <label>Email Address</label>
+              <input type="email"
+                placeholder="Enter your email address"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="input-contt">
+              <label>Password</label>
+              <div className='password'>
+                <input type={ShowPassword ? "text" : "password"}
+                  placeholder="Enter your password"
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                />
+                {
+                  ShowPassword ? (
+                    <FaEye
+                      style={{ width: "5%", cursor: "pointer" }}
+                      onClick={() => SetShowPassword(!ShowPassword)}
+                    />
+                  ) :
+                    (
+                      <FaEyeSlash
+                        style={{ width: "5%", cursor: "pointer" }}
+                        onClick={() => SetShowPassword(!ShowPassword)}
+                      />
+                    )
+                }
+
+              </div>
+              
+            </div>
+            <div className="input-contt">
+              <label>Confirm Password</label>
+              <div className='password'>
+                <input type={ShowPassword ? "text" : "password"}
+                  placeholder="Confirm Password"
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                />
+                {
+                  ShowPassword ? (
+                    <FaEye
+                      style={{ width: "5%", cursor: "pointer" }}
+                      onClick={() => SetShowPassword(!ShowPassword)}
+                    />
+                  ) :
+                    (
+                      <FaEyeSlash
+                        style={{ width: "5%", cursor: "pointer" }}
+                        onClick={() => SetShowPassword(!ShowPassword)}
+                      />
+                    )
+                }
+
+              </div>
+
+            </div>
+            <div className="checkbox-input">
+              <input type="checkbox" className="checkbox" />
+              <p className="remember-me-checkbox">I have read the <span>Terms and condition </span> and i agree</p>
+            </div>
+            <div className="button-cont">
+              <button>Sign up for free</button>
+            </div>
+
+            <article className="signup-line-cont">
+              <div className="firstline"></div>
+              <div className="text-or">or</div>
+              <div className="firstline"></div>
+            </article>
+
+
+            <button className="google" type="button">
+              <FcGoogle />
+              <p> Continue with Google</p>
+            </button>
+            <aside className="account-text">
+              <p>Already have an account? <span>Click here to login</span> </p>
+            </aside>
+
+
+          </form>
+        </div>
+
+
+
+      </div>
+
+      {/* <div className="signup-right">
+    
+
+        {successMessage && <p style={{ color: 'green', marginBottom: '10px' }}>{successMessage}</p>}
+
+       
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+          {formErrors.confirmPassword && <p className="error-text">{formErrors.confirmPassword}</p>}
+
+          <div className="terms">
+            <input
+              type="checkbox"
+              name="termsAccepted"
+              checked={formData.termsAccepted}
+              onChange={handleChange}
+            />
+            <span>
+              I have read the <a href="#">Terms and Conditions</a> and I agree
+            </span>
+          </div>
+          {formErrors.termsAccepted && <p className="error-text">{formErrors.termsAccepted}</p>}
+
+          <button type="submit" className="signup-btn">Sign-Up</button>
+        </form>
+      </div> */}
+    </div>
+  )
+}
+
+export default SignUpRunner
