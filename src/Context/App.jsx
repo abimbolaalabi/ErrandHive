5ac15903f1cb4ef43314aff2bff7455ba9dbff11
aninkud2 <adetadeservices@gmail.com>
2025-10-24 21:55:49 +0100

    api fixed

--- src/Context/App.jsx
+++ src/Context/App.jsx
@@ -1,15 +1,16 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 
 const AppContext = createContext(null);
 
 
 const AppProvider=({ children}) => {
+    const [user,setUser] = useState(null)
 
     return(
-        <AppContext.Provider >
+        <AppContext.Provider value={{user}}  >
             {children}
-        </AppContext.Provider>
+        </AppContext.Provider >
     )
 };
 
